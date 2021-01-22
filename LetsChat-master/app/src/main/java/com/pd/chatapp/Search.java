0bd649d5390f37c6b0572b14a15b36ac29bc992a
Rejabo <74000323+Rejabo@users.noreply.github.com>
2021-01-22 14:09:23 +0100

    Fahad version

--- LetsChat-master/app/src/main/java/com/pd/chatapp/Search.java
+++ LetsChat-master/app/src/main/java/com/pd/chatapp/Search.java
@@ -1,27 +1,108 @@
 package com.pd.chatapp;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.SearchView;
+
+import com.google.firebase.database.FirebaseDatabase;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
 
 public class Search extends AppCompatActivity {
 
-    private AlertDialog.Builder dialogBuilder;
-    private AlertDialog dialog;
-    private EditText fromCity, toCity, date, time;
-    private Button addTrip, cancel;
+
+    RecyclerView recview;
+    myadapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        setTitle("");
+
+        recview=(RecyclerView)findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("users"), model.class)
+                        .build();
+
+        adapter=new myadapter(options);
+        recview.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.searchmenu,menu);
+
+        MenuItem item=menu.findItem(R.id.search);
+
+        SearchView searchView=(SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                processsearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                processsearch(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    private void processsearch(String s)
+    {
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("users").orderByChild("course").startAt(s).endAt(s+"\uf8ff"), model.class)
+                        .build();
+
+        adapter=new myadapter(options);
+        adapter.startListening();
+        recview.setAdapter(adapter);
+
     }
 
 
@@ -38,44 +119,5 @@ public class Search extends AppCompatActivity {
             Intent i = new Intent(this, Search.class);
             startActivity(i);
         }
-        if (v.getId() == R.id.addButton){
-            createNewTripDialog();
-        }
-
     }
-
-    public void createNewTripDialog(){ //for creating new dialog
-        dialogBuilder = new AlertDialog.Builder(this);
-        final View tripPopupView = getLayoutInflater().inflate(R.layout.popup, null);
-        toCity = (EditText) tripPopupView.findViewById(R.id.toCity);
-        fromCity = (EditText) tripPopupView.findViewById(R.id.fromCity);
-        date = (EditText) tripPopupView.findViewById(R.id.date);
-        time = (EditText) tripPopupView.findViewById(R.id.time);
-
-        addTrip = (Button) tripPopupView.findViewById(R.id.addTripButton);
-        cancel = (Button) tripPopupView.findViewById(R.id.cancelButton);
-
-    dialogBuilder.setView(tripPopupView);
-    dialog= dialogBuilder.create();
-    dialog.show();
-
-    addTrip.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //define savebutton here
-
-        }
-    });
-
-    cancel.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            // define cancelbutton here
-            dialog.dismiss();
-        }
-    });
-
-    }
-
-
 }
