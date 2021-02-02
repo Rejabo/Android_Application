36ec71079dcaf537cf5667da4221c9303567370a
AlenGaz <32741272+AlenGaz@users.noreply.github.com>
2021-02-02 17:20:43 +0100

    update chat

--- Alen_Fahad/app/src/main/java/com/example/triptogether/Chat.java
+++ Alen_Fahad/app/src/main/java/com/example/triptogether/Chat.java
@@ -83,17 +83,17 @@ public class Chat extends AppCompatActivity {
                 Map map = dataSnapshot.getValue(Map.class);
                 String message = map.get("message").toString();
                 String userName = map.get("user").toString();
-                //System.out.println("kkkkkk");
+               
 
                 if(userName.equals(UserDetails.username)){
                     addMessageBox(message, 1);
-                    //System.out.println("in message1 box:" + message);
+                   
                 }
                 else{
                     addMessageBox(message, 2);
-                    //System.out.println("in message2 box:" + message);
+                  
                     makeNot(message, userName);
-                    //System.out.println("makeNot method");
+                 
                 }
             }
 
